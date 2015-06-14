a65be258843c8a884fd31e0dd61e0bbe21734802
Byron Lutz <byronlutz@gmail.com>
2015-06-13 18:27:36 -0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+	</div> <!-- End mainbody -->
+	
+<footer>
+<p class="contactfooter">
+(916)624-2108 | <a href="mailto:manager@rocklinestates.com">manager@rocklinestates.com</a> | 5505 S. Grove St., Rocklin, CA
+</p>
+<p>
+<a href="<?php echo get_permalink(34); ?>">Privacy Policy</a> | &copy; Rocklin Estates <?php echo date('Y'); ?>
+</p>
+<?php wp_footer(); ?>
+<script src="<?php echo bloginfo('template_directory'); ?>/js/jquery.placeholder.js"></script>
+</body>
+
+</html>
